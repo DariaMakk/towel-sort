0b6eaf155d09d57387935a172b25d05ae4b9fa0e
DariaMakk <dasha-nov13@rambler.ru>
2020-10-21 20:27:28 +0300

    fead: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 
 // You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return [];
+  let newMatrix = [];
+  function isEven(item, i) {
+     if (i % 2 !== 0) {
+        item = item.reverse();
+        newMatrix.push(item);
+        console.log('is even ', item)
+     } else {
+        console.log('is odd ', item)
+        newMatrix.push(item);
+     }
+  }
+  matrix.forEach(isEven)
+   newMatrix = [].concat.apply([], newMatrix);
+  return newMatrix;
 }
+
